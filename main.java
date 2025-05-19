c94ac8c6e4532415e88f801c93e306099749f7f2
Viresh <kariappavbk123@gmail.com>
2025-05-19 10:14:34 +0530

    Refactor: Comment out main method examples for logical and string operations

--- main.java
+++ main.java
@@ -70,66 +70,109 @@
 
 //Logical Operators - && , || , !
 // AND Operator - &&
-public class main {
-    public static void main(String args[]) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+// public class main {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 20;
+//         int c = 30;
+//         int d = 40;
 
-        System.out.println((a<b)&&(c<d)); //true
-        System.out.println((a<b)&&(c>d)); //false
-        System.out.println((a>b)&&(c>d)); //false
-    }
-}
+//         System.out.println((a<b)&&(c<d)); //true
+//         System.out.println((a<b)&&(c>d)); //false
+//         System.out.println((a>b)&&(c>d)); //false
+//     }
+// }
 
 
 //Or operator - ||
-public class main {
-    public static void main(String args[]) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+// public class main {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 20;
+//         int c = 30;
+//         int d = 40;
 
-        System.out.println((a<b)||(c<d)); //true
-        System.out.println((a<b)||(c>d)); //true
-        System.out.println((a>b)||(c>d)); //false
-    }
-}
+//         System.out.println((a<b)||(c<d)); //true
+//         System.out.println((a<b)||(c>d)); //true
+//         System.out.println((a>b)||(c>d)); //false
+//     }
+// }
 
 //Not operator - !
-public class main {
-    public static void main(String args[]) {
-        int a = 10;
-        int b = 20;
+// public class main {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 20;
 
-        System.out.println(!(a<b)); //false
-        System.out.println(!(a>b)); //true
-    }
-}
+//         System.out.println(!(a<b)); //false
+//         System.out.println(!(a>b)); //true
+//     }
+// }
 
- public class main {
-    public static void main(String args[]) {
-        String userName = "Coding";
+//  public class main {
+//     public static void main(String args[]) {
+//         String userName = "Coding";
 
-        System.out.println(userName.length());
-    }
-}
+//         System.out.println(userName.length());
+//     }
+// }
 
- public class main {
-    public static void main(String args[]) {
-        String userName = "Coding";
+//  public class main {
+//     public static void main(String args[]) {
+//         String userName = "Coding";
 
-        System.out.println(userName.toUpperCase());
-        System.out.println(userName.toLowerCase());
-    }
-}
+//         System.out.println(userName.toUpperCase());
+//         System.out.println(userName.toLowerCase());
+//     }
+// }
 
-public class main {
-    public static void main(String args[]) {
-        String userName = "Coding Circle";
+// public class main {
+//     public static void main(String args[]) {
+//         String userName = "Coding Circle";
 
-        System.out.println(userName.indexOf("Circle"));
-    }
-}
+//         System.out.println(userName.indexOf("Circle"));
+//     }
+// }
+
+
+//String Contination
+// public class main {
+//     public static void main(String args[]) {
+//         String fname = "Omkar";
+//         String lname = "Jadhav";
+
+//         // System.out.println(fname + " " + lname);
+//         System.out.println(fname.concat(lname));
+//     }
+// }
+
+// Math functions in Java
+// public class main {
+//     public static void main(String args[]) {
+//         // System.out.println(Math.max(5,8));
+//         // System.out.println(Math.min(5,8));
+//         // System.out.println(Math.sqrt(64));
+//         // System.out.println(Math.random()); // 0.0 - 0.9
+
+//         int random = (int)(Math.random() * 101);
+//         System.out.println(random);
+//     }
+// }
+
+//Taking user input using scanner
+//Scanner Class
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         //Object 
+//         Scanner scanner = new Scanner(System.in);
+
+//         //String
+//         System.out.println("Enter your name : ");
+//         String name = scanner.nextLine();
+
+//         System.out.println("Good Morning "+ name);
+//         scanner.close();
+//     }
+// }
