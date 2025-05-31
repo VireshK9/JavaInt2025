a3e97c8214dfcbf6b89c63158e9a22e27b85b7f7
Viresh <kariappavbk123@gmail.com>
2025-05-31 16:54:40 +0530

    Refactor: Comment out example code for control statements and loops

--- main.java
+++ main.java
@@ -14,15 +14,15 @@
 //     public static void main(String args[]) {
 //         String studentName = "John Doe";
 //         int studentId = 15;
-//         int studentAge = 23;
 //         float studentMarks = 75.25f;
 //         char studentGrade = 'B';
+//         boolean isPassed = true;
 
 //         System.out.println(studentName);
 //         System.out.println(studentId);
-//         System.out.println(studentAge);
 //         System.out.println(studentMarks);
 //         System.out.println(studentGrade);
+//         System.out.println(isPassed);
 //     }
 // }
 
@@ -176,3 +176,270 @@
 //         scanner.close();
 //     }
 // }
+
+//Control Statements
+// if else statement
+
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter your age : ");
+//         int age = scanner.nextInt();
+        
+//         if (age >= 18) {
+//             System.out.println("You are eligible to vote.");
+//         } else {
+//             System.out.println("You are not eligible to vote.");
+//         }
+//     }
+// }
+
+
+//if/else if/else statement
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter the 24hr time : ");
+//         int time = scanner.nextInt();
+
+//         if(time>=0 && time<12){
+//             System.out.println("Good Morning");
+//         }
+//         else if(time>=12 && time<16){
+//             System.out.println("Good Afternoon");
+//         }
+//         else if(time>=16 && time<20){
+//             System.out.println("Good Evening");
+//         }
+//         else if(time>=20 && time<24){
+//             System.out.println("Good Night");
+//         }
+//         else{
+//             System.out.println("Invalid Time");
+//         }
+//     }
+// }
+
+//Short hand if/else statement
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter your age : ");
+//         int age = scanner.nextInt();
+
+//         String result = (age>=30) ? "Rs.600" : "Rs.300";
+//         System.out.println("Your ticket price is: " + result);
+//     }
+// }
+
+//Switch case statement
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter the day number of week : ");
+//         int day = scanner.nextInt();
+
+//         switch(day){
+//             case 1:
+//                 System.out.println("Monday");
+//                 break;
+//             case 2:
+//                 System.out.println("Tuesday");
+//                 break;
+//             case 3:
+//                 System.out.println("Wednesday");
+//                 break;
+//             case 4:
+//                 System.out.println("Thursday");
+//                 break;
+//             case 5:
+//                 System.out.println("Friday");
+//                 break;
+//             case 6:
+//                 System.out.println("Saturday");
+//                 break;
+//             case 7:
+//                 System.out.println("Sunday");
+//                 break;
+//             default:
+//                 System.out.println("Invalid day number");
+//         }
+//     }
+// }
+
+// Nested if/else
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter your age : ");
+//         int age = scanner.nextInt();
+
+//         if(age>=60){
+//             System.out.println("You have to pay Rs.600");
+//         }
+//         else if(age>=20){
+//             System.out.println("Do you want a costume ?\n1.Yes\n2.No\n");
+//             int choice = scanner.nextInt();
+//             if(choice==1){
+//                 System.out.println("Ticket = Rs.400\nCostume = Rs.200\nTotal = Rs.600");
+//             }
+//             else{
+//                 System.out.println("Ticket = Rs.400\nCostume = Rs.0\nTotal = Rs.400");
+//             }
+//         }
+//         else{
+//             System.out.println("You have to pay Rs.200");
+//         }
+//     }
+// }
+
+
+// For loop
+
+// public class main {
+//     public static void main(String[] args) {
+//         for(int i=1;i<=5;i++){
+//             System.out.println(i);
+//             for(int j=100;j<=105;j++){
+//                 System.out.println("  " + j);
+//             }
+//         }
+//     }
+// }
+
+//Syntax of for loop
+// for(initialization; condition; increment/decrement){
+//     //code block
+// }
+
+// for each loop
+// public class main {
+//     public static void main(String[] args) {
+//         String[] names = {"Omkar", "Shivam", "Sagar", "Siddharth", "Akash"};
+        
+//         for(String name : names){
+//             System.out.print(name + " ");
+//         }
+//     }
+// }
+
+// while loop
+// public class main {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while(i<=10){
+//             System.out.println(i);
+//             i++;    //i = i + 1
+//         }
+//     }
+// }
+
+//do while loop
+// public class main {
+//     public static void main(String[] args) {
+//         int i = 24;
+
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }while(i<=10);
+        
+//     }
+// }
+
+// Break and continue statement
+//break statement
+// public class main {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while(i<=10){
+//             if(i==5){
+//                 break;
+//             }
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+//continue statement
+// public class main {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while(i<=10){
+//             i++;
+//             if(i==5){
+//                 continue;
+//             }
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+// Arrays
+// public class main {
+//     public static void main(String[] args) {
+//         int[] num = {1,2,3,4,5,6,7,8,9,10};  
+        
+//         System.out.println(num[5]);
+//     }
+// }
+
+// public class main {
+//     public static void main(String[] args) {
+//         String[] names = {"Omkar", "Shivam", "Sagar", "Siddharth", "Akash"};
+//         // names[2] = "Raj";
+
+//         // System.out.println(names[2]);
+//         System.out.println(names.length);
+//     }
+// }
+
+
+// looping through an array
+// public class main {
+//     public static void main(String[] args) {
+//         String[] names = {"Omkar", "Shivam", "Sagar", "Siddharth", "Akash"};
+//         for(int i=0;i<names.length;i++){
+//             System.out.println(names[i]);
+//         }
+//     }
+// }
+
+// Multidimensional Arrays
+// public class main {
+//     public static void main(String[] args) {
+//         // 2D Array
+//         int[][] num = {
+//             {1, 2, 3},
+//             {4, 5, 6},
+//             {7, 8, 9}
+//         };
+
+//         // Looping through a 2D array
+//         for(int i=0;i<num.length;i++){
+//             for(int j=0;j<num[i].length;j++){
+//                 System.out.println(num[i][j]);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
